b930814a11a0910c9ba52146fefb6c642c796a74
aidan <aapaetsch@gmail.com>
2019-02-11 21:22:42 +0000

    Initial build

---
+++ precache-manifest.cdaa344237b0ae0ed7b683267a75720e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3c57c01e7c88d792d2ba",
+    "url": "/static/js/main.3c57c01e.chunk.js"
+  },
+  {
+    "revision": "992eeab57fe488dbaf35",
+    "url": "/static/js/1.992eeab5.chunk.js"
+  },
+  {
+    "revision": "3c57c01e7c88d792d2ba",
+    "url": "/static/css/main.2c28baf6.chunk.css"
+  },
+  {
+    "revision": "2658c25c93fdad5f05b21fe35de7448a",
+    "url": "/index.html"
+  }
+];
